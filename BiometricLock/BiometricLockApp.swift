7ceb3633386cbd25e2b7665b60050f75ff4a571c
IvanL <antila3567@gmail.com>
2023-12-24 13:06:23 +0200

    Initial Commit

---
+++ BiometricLock/BiometricLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricLockApp.swift
+//  BiometricLock
+//
+//  Created by Иван Легенький on 24.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BiometricLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
